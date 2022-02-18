eaad2a1179631d77033d49a1a7710661a0276db5
JeffreyJCZY <jeffreyzhiyeechong@gmail.com>
2022-02-18 23:51:37 +0700

    Jeffrey Add profile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Homepage from './page/Homepage';
 import ItemDetail from './page/ItemDetail';
 import AddNewItem from './page/AddNewItem';
+import Profile from './page/Profile';
 import ReactFirebaseFileUpload from './page/ReactFirebaseFileUpload';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
@@ -16,7 +17,7 @@ function App() {
         <Container>
           <Navbar.Brand href="/lendit">LENDIT</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Profile</Nav.Link>
+            <Nav.Link href='/Profile/6310023'>Profile</Nav.Link>
             <Nav.Link href="#features">Items</Nav.Link>
             <Nav.Link href="#pricing">History</Nav.Link>
           </Nav>
@@ -26,12 +27,13 @@ function App() {
         </Container>
       </Navbar>
 
-    
+
       <Router>
         <Routes>
-          <Route exact path="/lendit" element={ <Homepage/>}/>
-          <Route exact path="/itemDetail/:id" element={ <ItemDetail/>} />
-          <Route exact path="/addItem" element={<AddNewItem/>}/>
+          <Route exact path="/lendit" element={<Homepage />} />
+          <Route exact path="/itemDetail/:id" element={<ItemDetail />} />
+          <Route exact path="/addItem" element={<AddNewItem />} />
+          <Route exact path="/Profile/:userid" element={<Profile />} />
         </Routes>
       </Router>
       {/* <div className="box">
